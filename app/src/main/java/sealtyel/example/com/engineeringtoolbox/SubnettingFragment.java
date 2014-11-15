f34dd251e1204f2ed27956ccd0bf94eaa178bda9
Mayra Lizarraga <mayral.lizarraga@gmail.com>
2014-11-14 17:06:11 -0800

    subnettingfragment agregado al menu

--- app/src/main/java/sealtyel/example/com/engineeringtoolbox/SubnettingFragment.java
+++ app/src/main/java/sealtyel/example/com/engineeringtoolbox/SubnettingFragment.java
@@ -1,44 +1,58 @@
 package sealtyel.example.com.engineeringtoolbox;
 
 import android.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.*;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import sealtyel.example.com.engineeringtoolbox.subnetting.ElementoRed;
+import sealtyel.example.com.engineeringtoolbox.subnetting.ListRedesAdapter;
 
 
 public class SubnettingFragment extends Fragment {
 
     public SubnettingFragment(){}
 
+    int numeroRed=0;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ArrayList<ElementoRed> values;
+        final ListRedesAdapter adapter;
 
         View rootView = inflater.inflate(R.layout.fragment_subnetting, container, false);
+
 
         Button botonCalcular = (Button) rootView.findViewById(R.id.buttonCalcularRed);
         final EditText red = (EditText) rootView.findViewById(R.id.editTextRed);
         final EditText mascara = (EditText) rootView.findViewById(R.id.editTextMask);
         Button botonAgregar = (Button) rootView.findViewById(R.id.buttonAgregarRed);
-        ListView listaRedes = (ListView) rootView.findViewById(R.id.listViewRedes);
+        final ListView listaRedes = (ListView) rootView.findViewById(R.id.listViewRedes);
+        values =  new ArrayList<ElementoRed>();
+        adapter = new ListRedesAdapter(getActivity().getApplication(), values);
+
+        listaRedes.setAdapter(adapter);
 
         botonAgregar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast toast1 = android.widget.Toast.makeText(getActivity().getApplicationContext(),"Add",android.widget.Toast.LENGTH_SHORT);
                 toast1.show();
+                adapter.notifyDataSetChanged();
+                ElementoRed red=new ElementoRed();
+                red.setTitulo("Red "+contador());
+                adapter.add(red);
+                listaRedes.setAdapter(adapter);
+
             }
-
-
         });
 
         botonCalcular.setOnClickListener(new View.OnClickListener() {
@@ -46,8 +60,6 @@ public class SubnettingFragment extends Fragment {
                 Toast toast1 = android.widget.Toast.makeText(getActivity().getApplicationContext(),"Calcular",android.widget.Toast.LENGTH_SHORT);
                 toast1.show();
             }
-
-
         });
 
         red.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -59,12 +71,14 @@ public class SubnettingFragment extends Fragment {
                     Matcher m = p.matcher(red.getText().toString());
                     if (!m.matches())
                         red.setError("Formato de ip incorrecto");
-
                 }
             }
         });
 
-
         return rootView;
+    }
+
+    public int contador(){
+        return ++numeroRed;
     }
 }
